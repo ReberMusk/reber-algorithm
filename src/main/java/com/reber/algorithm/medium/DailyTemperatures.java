fa0b29dc5ff2e1d2291a2194287a6fff0e25371a
tongyuan <tongyuan@gaoding.com>
2022-12-19 09:59:25 +0800

    feat: 739. Daily Temperatures

---
+++ src/main/java/com/reber/algorithm/medium/DailyTemperatures.java
@@ -0,0 +1,74 @@
+package com.reber.algorithm.medium;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: temperatures = [73,74,75,71,69,72,76,73]
+ * 输出: [1,1,4,2,1,1,0,0]
+ * 示例 2:
+ *
+ * 输入: temperatures = [30,40,50,60]
+ * 输出: [1,1,1,0]
+ * 示例 3:
+ *
+ * 输入: temperatures = [30,60,90]
+ * 输出: [1,1,0]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/daily-temperatures
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Tong yuan
+ * @since 2022-12-18 23:07:00
+ */
+public class DailyTemperatures {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(dailyTemperatures(new int[]{73,74,75,71,69,72,76,73})));
+        System.out.println(Arrays.toString(dailyTemperatures2(new int[]{73,74,75,71,69,72,76,73})));
+    }
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (temperatures[j] < temperatures[i]) {
+                   continue;
+                }
+                if (temperatures[j] > temperatures[i]) {
+                    res[i] = j - i;
+                    break;
+                } else {
+                    res[i] = 0;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static int[] dailyTemperatures2(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        res[temperatures.length - 1] = 0;
+        for (int i = temperatures.length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < temperatures.length; j += res[j]) {
+                if (temperatures[i] < temperatures[j]) {
+                    res[i] = j - i;
+                    break;
+                } else if (res[j] == 0) {
+                    res[i] = 0;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+
+}
