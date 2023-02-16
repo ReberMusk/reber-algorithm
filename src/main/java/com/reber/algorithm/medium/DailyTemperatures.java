b8d551d43a59b21b4fcd4ae14e7e25089ed4dd74
tongyuan <tongyuan@gaoding.com>
2023-02-16 18:52:25 +0800

    feat: 按 chatGPT 的建议优化

--- src/main/java/com/reber/algorithm/medium/DailyTemperatures.java
+++ src/main/java/com/reber/algorithm/medium/DailyTemperatures.java
@@ -33,6 +33,7 @@ public class DailyTemperatures {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(dailyTemperatures(new int[]{73,74,75,71,69,72,76,73})));
         System.out.println(Arrays.toString(dailyTemperatures2(new int[]{73,74,75,71,69,72,76,73})));
+        System.out.println(Arrays.toString(dailyTemperatures3(new int[]{73,74,75,71,69,72,76,73})));
     }
 
     public static int[] dailyTemperatures(int[] temperatures) {
