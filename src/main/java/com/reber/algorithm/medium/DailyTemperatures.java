230743f8a383d0528ee5be6ab97f32147557d82a
tongyuan <tongyuan@gaoding.com>
2023-02-16 18:47:55 +0800

    feat: 按 chatGPT 的建议优化

--- src/main/java/com/reber/algorithm/medium/DailyTemperatures.java
+++ src/main/java/com/reber/algorithm/medium/DailyTemperatures.java
@@ -1,6 +1,7 @@
 package com.reber.algorithm.medium;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
@@ -68,6 +69,20 @@ public class DailyTemperatures {
             }
         }
         return res;
+    }
+
+    public static int[] dailyTemperatures3(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int preIndex = stack.pop();
+                ans[preIndex] = i - preIndex;
+            }
+            stack.push(i);
+        }
+        return ans;
     }
 
 
