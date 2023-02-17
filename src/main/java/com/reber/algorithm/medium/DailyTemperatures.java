06bf03a9d1f2e88db1354515b47838f8a151ec2a
tongyuan <tongyuan@gaoding.com>
2023-02-17 10:35:14 +0800

    feat: test pr

--- src/main/java/com/reber/algorithm/medium/DailyTemperatures.java
+++ src/main/java/com/reber/algorithm/medium/DailyTemperatures.java
@@ -87,4 +87,22 @@ public class DailyTemperatures {
     }
 
 
+
+    public static int[] dailyTemperatures4(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        res[temperatures.length - 1] = 0;
+        for (int i = temperatures.length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < temperatures.length; j += res[j]) {
+                if (temperatures[i] < temperatures[j]) {
+                    res[i] = j - i;
+                    break;
+                } else if (res[j] == 0) {
+                    res[i] = 0;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
 }
